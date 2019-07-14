fcf27ea68b1c20bcbae0bd4d48e9a567b7b793ae
Lisa V <lisaqv@bu.edu>
2019-07-13 23:50:25 -0400

    Updates

---
+++ precache-manifest.660c70625c1c62c65df00091e4304b4d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3809803036a931d74dc9df9b4f0559bb",
+    "url": "/LessTrash/index.html"
+  },
+  {
+    "revision": "6eda3aca344435c05bb6",
+    "url": "/LessTrash/static/css/2.14686bf5.chunk.css"
+  },
+  {
+    "revision": "8e6f46c4a26e971d2f36",
+    "url": "/LessTrash/static/css/main.296fbec7.chunk.css"
+  },
+  {
+    "revision": "6eda3aca344435c05bb6",
+    "url": "/LessTrash/static/js/2.dc49d2a5.chunk.js"
+  },
+  {
+    "revision": "8e6f46c4a26e971d2f36",
+    "url": "/LessTrash/static/js/main.4b5bbc08.chunk.js"
+  },
+  {
+    "revision": "c4ad602352cd01406937",
+    "url": "/LessTrash/static/js/runtime~main.33962fa5.js"
+  },
+  {
+    "revision": "a2ef5859a9055429d97c5ec8eafba37c",
+    "url": "/LessTrash/static/media/coin.a2ef5859.png"
+  },
+  {
+    "revision": "21a09852c80049d51a9a6180635adba4",
+    "url": "/LessTrash/static/media/logo.21a09852.png"
+  },
+  {
+    "revision": "4a1f8e4b540852d2fda4b6609f16964b",
+    "url": "/LessTrash/static/media/slider.4a1f8e4b.png"
+  }
+]);
